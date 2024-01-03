b590dee6d9b0d8de66186dad766ca21cfcd9d353
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-03 12:08:02 +0530

    firstly creare Models for Student...

---
+++ ReactAspCrud/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactAspCrud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public String stname { get; set; }
+        public String course { get; set; }
+    }
+}
