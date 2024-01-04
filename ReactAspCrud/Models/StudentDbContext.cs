7b11ab32e20eb873638ad78977480e75eb6bff34
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-04 21:36:53 +0530

    configuring the DbContext to use SQL Server as the databse provider............. & connection string............. & migration......... & create the API(in Controller)...........

---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; initial Catalog=lbs; User Id=sa; password=123; TrustServerCertificate=True");
+        }
+    }
+}
--- ReactAspCrud/Program.cs
+++ ReactAspCrud/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReactAspCrud.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// **********************
+
+builder.Services.AddDbContext<StudentDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("StudentDbContext")));
+
+// **********************
+
 
 var app = builder.Build();
 
